0d714b9e536ae429655ee24aaeee59f94ccbac7d
noxydoesthings <115258075+noxydoesthings@users.noreply.github.com>
2022-12-09 22:09:55 +0700

    Create god.lua
    :trollface:

---
+++ god.lua
@@ -0,0 +1,12 @@
+--// i made this script in literally a couple minutes lmao
+local lp = game.Players.LocalPlayer.Character
+
+local function god()
+   lp.CharacterDied:Destroy()
+       lp.Death:Destroy()
+       lp.Ragdolled:Destroy()
+       lp.BodyEffects:Destroy()
+end
+
+
+god()
