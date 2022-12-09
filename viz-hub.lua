e11c45c19e2ea14ae68b8a461ab40e2c3268931c
noxydoesthings <115258075+noxydoesthings@users.noreply.github.com>
2022-12-09 22:07:09 +0700

    Update viz-hub.lua
    added chatlog

--- viz-hub.lua
+++ viz-hub.lua
@@ -1,6 +1,5 @@
 local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/cypherdh/VanisUILIB/main/.gitignore'))()
-local v3 = game.Players.LocalPlayer.Character
-local Window = library:CreateWindow("D/Tha Hood", "V.1", 10044538000)
+local Window = library:CreateWindow("Viz Hub", "dha hood/tha hood hax", 10044538000)
 
 local Tab = Window:CreateTab("Scripts")
 
@@ -16,29 +15,8 @@ local function g_()
        v3.Ragdolled:Destroy()
        v3.BodyEffects:Destroy()
 end
-
-local function invis()
-if ch:FindFirstChild("R6Convert") or ch:FindFirstChild("Bear Suit") then
-    local mypos = ch.HumanoidRootPart.CFrame
-    ch.HumanoidRootPart.CFrame = CFrame.new(0,10000000,0)
-    ch.HumanoidRootPart.Velocity = Vector3.new(0,50,0) -- prevent fall damage in some shitty game
-    task.wait(0.5)
-    for i,v in pairs(ch:GetChildren()) do
-        if v:IsA("Accessory") or v:IsA("Accoutrement") then
-            if v.Name == "R6Convert" or v.Name == "Bear Suit" then
-                pcall(function()
-                    v.Handle.Anchored = true
-                    v.Handle.AccessoryWeld:Destroy()
-                end)
-            end
-        end    
-    end
-    ch.HumanoidRootPart.CFrame = mypos
-end
-end
-
 --// things down here
-Button = Page:CreateButton("Cash Graber", "Grab Cash for you. ", function()
+Button = Page:CreateButton("Cash Graber", "Grab Cash for you. (rejoin to disable)", function()
 local LP = game.Players.LocalPlayer
 while game.RunService.Heartbeat:Wait()do
 local success, err = pcall(function()
@@ -58,6 +36,68 @@ end)
 Button = Page:CreateButton("God", "Makes you immortal to guns.", function()
     g_()
 end)
+
+Button = Page:CreateButton("Enable Chat", "Enables the chat.", function()
+    enabled = true
+    spyOnMyself = true
+    public = false
+    publicItalics = true
+    privateProperties = {
+	Color = Color3.fromRGB(0,255,255); 
+	Font = Enum.Font.SourceSansBold;
+	TextSize = 18;
+}
+--////////////////////////////////////////////////////////////////
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local saymsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
+local getmsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("OnMessageDoneFiltering")
+local instance = (_G.chatSpyInstance or 0) + 1
+_G.chatSpyInstance = instance
+
+local function onChatted(p,msg)
+	if _G.chatSpyInstance == instance then
+		if p==player and msg:lower():sub(1,4)=="/spy" then
+			enabled = not enabled
+			wait(0.3)
+			privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+			StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+		elseif enabled and (spyOnMyself==true or p~=player) then
+			msg = msg:gsub("[\n\r]",''):gsub("\t",' '):gsub("[ ]+",' ')
+			local hidden = true
+			local conn = getmsg.OnClientEvent:Connect(function(packet,channel)
+				if packet.SpeakerUserId==p.UserId and packet.Message==msg:sub(#msg-#packet.Message+1) and (channel=="All" or (channel=="Team" and public==false and Players[packet.FromSpeaker].Team==player.Team)) then
+					hidden = false
+				end
+			end)
+			wait(1)
+			conn:Disconnect()
+			if hidden and enabled then
+				if public then
+					saymsg:FireServer((publicItalics and "/me " or '').."{SPY} [".. p.Name .."]: "..msg,"All")
+				else
+					privateProperties.Text = "{SPY} [".. p.Name .."]: "..msg
+					StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+				end
+			end
+		end
+	end
+end
+
+for _,p in ipairs(Players:GetPlayers()) do
+	p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+end
+Players.PlayerAdded:Connect(function(p)
+	p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+end)
+privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+local chatFrame = player.PlayerGui.Chat.Frame
+chatFrame.ChatChannelParentFrame.Visible = true
+chatFrame.ChatBarParentFrame.Position = chatFrame.ChatChannelParentFrame.Position+UDim2.new(UDim.new(),chatFrame.ChatChannelParentFrame.Size.Y)
+end)
+
 
 Page:CreateSlider("Walkspeed", 16, 500,function(arg)
    v3.Humanoid.WalkSpeed = (arg)
